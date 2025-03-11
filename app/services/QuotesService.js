23eb0f8faf899ca383c040bd20811e66105bf350
AdamRB208 <adamrborup@gmail.com>
2025-03-10 23:49:30 -0600

    final

--- app/services/QuotesService.js
+++ app/services/QuotesService.js
@@ -10,12 +10,7 @@ class QuotesService {
     console.log('got quotes', response.data)
     const quote = new Quotes(response.data)
     AppState.quotes = quote
-    // const quote = response.data.map(quotesPOJO => new Quotes(quotesPOJO))
-    // AppState.quotes = quote
-    // console.log('storing quotes', quote)
-    // const quotes = [];
-    // quotes.push("quote", "author", "source");
-    // console.log('pushing quotes', quotes.length);
+
   }
 
 }
